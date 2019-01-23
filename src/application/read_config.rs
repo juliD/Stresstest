fe71ac94a4f82b32968bcd7723abd3e6be9c7b35
Julia <ru76kim@mwn.eduroam.de>
2019-01-23 14:47:55 +0100

    watching config file now working

--- src/application/read_config.rs
+++ src/application/read_config.rs
@@ -6,6 +6,9 @@ extern crate notify;
 use std::collections::HashMap;
 use config::*;
 use std::sync::RwLock;
+use notify::{RecommendedWatcher, DebouncedEvent, Watcher, RecursiveMode};
+use std::sync::mpsc::channel;
+use std::time::Duration;
 
 const CONFIG_FILE_NAME: &str = "Settings";
 
@@ -26,28 +29,54 @@ lazy_static! {
     });
 }
 
-fn to_hashmap() {
+fn settings_to_hashmap() -> HashMap<String, String> {
     SETTINGS
         .read()
         .unwrap()
         .clone()
         .try_into::<HashMap<String, String>>()
-        .unwrap();
+        .unwrap()
 }
 
+
 pub fn parse_config() {
-    let mut settings = config::Config::default();
-    settings
-        // Add in `./Settings.toml`
-        .merge(config::File::with_name(CONFIG_FILE_NAME)).unwrap();
-    
-    let settings_hash = SETTINGS.read().clone().try_into::<HashMap<String, String>>().unwrap();
+    let settings_hash = settings_to_hashmap();
     let config_keys: Vec<&str> = vec![CONFIG_KEY_HOSTS, CONFIG_KEY_TARGET, CONFIG_KEY_MASTER_SOCKET, CONFIG_KEY_REQUESTS, CONFIG_KEY_MILLISECONDS];
+
 
     //check if all necessary keys are contained in config file
     match check_settings_exist(config_keys, &settings_hash){
         Ok(()) => println!("Config file complete"),
         Err(n) => println!("Error: Key: {} is missing",n)
+    }
+}
+
+
+pub fn watch(){
+    //channel to receive events
+    let (tx, rx) = channel();
+
+    //create watcher
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_secs(2)).unwrap();
+
+    //add path to be watched. 
+    watcher.watch("./Settings.toml", RecursiveMode::NonRecursive).unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(DebouncedEvent::Write(_)) => {
+                println!(" * Settings.toml written; refreshing configuration ...");
+                SETTINGS.write().unwrap().refresh().unwrap();
+                parse_config();
+
+            }
+
+            Err(e) => println!("watch error: {:?}", e),
+
+            _ => {
+                // Ignore event
+            }
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate lazy_static;
 mod application;
 mod examples;
 fn main() {
-    application::read_config::show();
+    application::read_config::watch();
     //examples::split_computation::run();
     // examples::spawning_child_actors::run();
 }
