de655f13044a8bcaf31135fdcf750c7f4dca5f51
lx4r <hallo@l3r.de>
2019-01-26 16:33:13 +0100

    small refactoring in router

--- src/router.rs
+++ src/router.rs
@@ -10,6 +10,10 @@ use crate::message::Envelope;
 use crate::context::*;
 use crate::address::*;
 
+const CHANNEL_BUFFER_SIZE: usize = 1_024;
+const SYSTEM_STARTING_MESSAGE: &str = "setting up system";
+const SYSTEM_STARTUP_FINISHED_MESSAGE: &str = "setup done";
+
 pub struct Router {}
 
 impl Router {
@@ -18,40 +22,35 @@ impl Router {
         F: FnOnce() + 'static + Send,
     {
         TokioUtil::run_blocking(move || {
-            println!("setting up system");
+            println!("{}", SYSTEM_STARTING_MESSAGE);
             f();
-            println!("setup done");
+            println!("{}", SYSTEM_STARTUP_FINISHED_MESSAGE);
         });
 
+        // TODO: remove?
         // run blocking stream to keep system alive
         // println!("run blocking stream to keep system alive");
         // let (_, system_receiver) = channel::<Envelope>(8);
         // TokioUtil::handle_stream_blocking(system_receiver, move |_| {});
     }
 
-    pub fn register_actor<A>(actor: A, parent_address: Option<Address>) -> Address
+    pub fn register_actor<A>(mut actor: A, parent_address: Option<Address>) -> Address
     where
         A: Actor + Send + 'static,
     {
-        let (sender, receiver) = channel::<Envelope>(16);
+        let (sender, receiver) = channel::<Envelope>(CHANNEL_BUFFER_SIZE);
 
         let child_address = Address { sender: sender };
-
-        let mut actor_ref = actor;
-
-        let parent_address_clone = match parent_address {
-            Some(addr) => Some(addr.clone()),
-            None => None,
-        };
+        let parent_address_clone = parent_address.map(|address: Address| address.clone());
 
         let context = Context {
             parent_address: parent_address_clone,
             own_address: child_address.clone(),
         };
-        actor_ref.receive_context(context);
+        actor.receive_context(context);
 
         TokioUtil::handle_stream_background(receiver, move |envelope| {
-            actor_ref.handle(envelope.message);
+            actor.handle(envelope.message);
         });
         child_address
     }
