d6a7c8b8b419cb199ac5492af9a775eb62bdc19e
Julia <doernerj@cip.ifi.lmu>
2019-01-31 18:37:48 +0100

    http requests and target etc

--- src/application/master.rs
+++ src/application/master.rs
@@ -14,9 +14,8 @@ use hyper::rt::{self, Future};
 
 struct WorkerActor{
     id: u32,
-    children: LinkedList<Address>,
-    child_id_counter: u32,
     context: Option<Context>,
+    target: hyper::Uri,
 }
 impl WorkerActor{
 
@@ -24,37 +23,59 @@ impl WorkerActor{
 
 impl Actor for WorkerActor{
     fn handle(&mut self, message: String){
-        
-        match message.as_ref(){
+        let mut split = message.split(" ");
+        let vec: Vec<&str> = split.collect();
+
+        match vec[0]{
             "start" =>{
-                println!("I started");
-                // rt::run(rt::lazy(move || {
-                //     // This is main future that the runtime will execute.
-                //     //
-                //     // The `lazy` is because we don't want any of this executing *right now*,
-                //     // but rather once the runtime has started up all its resources.
-                //     //
-                //     // This is where we will setup our HTTP client requests.
-                //     let client = Client::new();
-                //     let uri = "http://httpbin.org/ip".parse().unwrap();
-                //     client
-                //         .get(uri)
-                //         .map(move |res| {
-                //             println!("ID: {} HTTP Response {}", self.id, res.status());
-                //         })
-                //         .map_err(|err| {
-                //             println!("Error: {}", err);
-                //         })
-                // }));
+                let ctx: &Context = self.context.as_ref().expect("");
+                let paddr: &Address = ctx.parent_address.as_ref().expect("");
+                let own_addr: &Address = &ctx.own_address;
+                for _ in 0..10{
+                
+                    rt::spawn(rt::lazy(move || {
+                        let client = Client::new();
+                        let uri = "http://httpbin.org/ip".parse().unwrap();
+                        client
+                            .get(uri)
+                            .map(move |res| {
+                                println!("HTTP Response {}", res.status());
+                            })
+                            .map_err(|err| {
+                                println!("Error: {}", err);
+                            })
+                    }));                    
+                    paddr.send("1".to_owned());                    
+                }                
+                own_addr.send("start".to_owned());
+            }
+            "target" =>{
+                self.target = vec[1].parse().unwrap();
             }
             "stop" =>{
-
+                println!("Stopped");
             }
             _=>{
 
             }
         }
     }
+    fn receive_context(&mut self, context: Context){
+        self.context = Some(context);
+    }
+}
+
+struct LogActor{
+    id: u32,
+    context: Option<Context>,
+}
+
+impl Actor for LogActor{
+
+    fn handle(&mut self, message: String){
+
+    }
+
     fn receive_context(&mut self, context: Context){
         self.context = Some(context);
     }
@@ -73,6 +94,24 @@ impl MasterActor{
             context: None,
         }
     }
+  
+}
+
+fn broadcast_children(list: &LinkedList<Address>, count: u32, message: String){
+    let mut iter = list.iter();
+    iter.next();
+
+    for _ in 0..count-1 {
+        match iter.next(){
+            Some(c) =>{
+                c.send(message.to_owned());
+            },
+            _ =>{
+
+            }
+        }
+
+    }
 }
 
 impl Actor for MasterActor{
@@ -83,25 +122,19 @@ impl Actor for MasterActor{
             
             match vec[1] {
                 "start" => {
-                    self.child_id_counter += 1;
-                    let ctx: &Context = self.context.as_ref().expect("");
-                    let child_addr = ctx.register_actor(WorkerActor {
-                        children: LinkedList::new(),
-                        id: self.child_id_counter,
-                        child_id_counter: 0,
-                        context: None,
-                    });
-                    self.children.push_front(child_addr.clone());
-                    child_addr.send("start".to_owned());
+                    //TODO: send all worker children start message
+                    broadcast_children(&self.children, self.child_id_counter, "start".to_string());
+                    
                 }
                 "stop" => {
-
+                    broadcast_children(&self.children, self.child_id_counter, "stop".to_string());
                 }
                 "log" =>{
 
                 }
                 "target" =>{
-
+                    //TODO:check if target correct
+                    broadcast_children(&self.children, self.child_id_counter, message);
                 }
                 "help" =>{
                     println!("--------------------------------------------------------------------------");
@@ -122,22 +155,49 @@ impl Actor for MasterActor{
         self.context = Some(context);
         println!("init MasterActor");
 
+
+
         //only if master process
+
+        //Input Actor
         self.child_id_counter += 1;
         let ctx: &Context = self.context.as_ref().expect("");
         let child_addr = ctx.register_actor(InputActor {
             id: self.child_id_counter,
             context: None,
         });
-        self.children.push_front(child_addr.clone());
+        self.children.push_back(child_addr.clone());
         child_addr.send("watch input".to_owned());
 
-        //TODO:create LogActor
+        
+        //TODO: LogActor
+        // self.child_id_counter += 1;
+        // let ctx: &Context = self.context.as_ref().expect("");
+        // let child_addr = ctx.register_actor(LogActor {
+        //     id: self.child_id_counter,
+        //     context: None,
+        // });
+        // self.children.push_front(child_addr.clone());
 
         //end if master process
 
 
+        //create as many actors as cores available
+        let num = num_cpus::get();
+        for x in 0..num {
+            self.child_id_counter += 1;
+            let ctx: &Context = self.context.as_ref().expect("");
+            let child_addr = ctx.register_actor(WorkerActor {
+                        id: self.child_id_counter,
+                        context: None,
+                        target: "http://httpbin.org/ip".parse().unwrap(),
+                    });
+            self.children.push_back(child_addr.clone());
+        }
+
     }
+
+    
 }
 
 struct InputActor {
@@ -169,6 +229,8 @@ impl Actor for InputActor{
         println!("init InputActor");
     }
 }
+
+
 
 pub fn run() {
     println!("init");
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod examples;
 mod application;
 
+extern crate num_cpus;
+
 fn main() {
     
     // examples::split_computation::run();
     // examples::spawning_child_actors::run();
     // examples::tcp_stream::run();
     //examples::spawn_on_tcp_message::run();
+    
+    
+
     application::master::run();
 }
