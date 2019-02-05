b9bf076f59ebd5eda16302e7b0eef44c2ed2249e
Konrad Fischer <konrad.fischer@campus.lmu.de>
2019-02-05 16:21:42 +0100

    generic message receiving function

--- src/examples/tcp_duplex.rs
+++ src/examples/tcp_duplex.rs
@@ -1,12 +1,16 @@
+// TODO: slave only has to listen to ONE master
+// TODO: simplify master code and reuse listening from slave
+
+extern crate actor_model;
 extern crate byteorder;
 
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::env;
+use std::io;
 use std::io::{Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::str::from_utf8;
 use std::thread;
-use std::io;
 
 pub fn run() {
   let args: Vec<String> = env::args().collect();
@@ -22,43 +26,49 @@ pub fn run() {
   let master_ip = "127.0.0.1";
   let master_port = 3000;
 
-  let number_of_actors: u32 = 5;
-  let mut number_of_actors_bytes = vec![];
-  number_of_actors_bytes.write_u32::<BigEndian>(number_of_actors).unwrap();
+  let number_of_actors = "5";
+  /*   let mut number_of_actors_bytes = vec![];
+  number_of_actors_bytes.write_u32::<BigEndian>(number_of_actors).unwrap(); */
 
   if is_master_bool {
     // master is connecting to slave
 
     match TcpStream::connect("localhost:3333") {
       Ok(mut stream) => {
-        println!("connected to slave on port 3333");
+        /* println!("connected to slave on port 3333");
 
-        let msg = b"Hello!";
-        //let msg = &number_of_actors_bytes[..];
+          let msg = b"Hello!";
+          //let msg = &number_of_actors_bytes[..];
 
-        stream.write(msg).unwrap();
-        println!("Sent {}, awaiting reply...", number_of_actors);
+          stream.write(msg).unwrap();
+          println!("Sent {}, awaiting reply...", number_of_actors);
 
-        let mut data = [0 as u8; 6]; // using 6 byte buffer
-        match stream.read_exact(&mut data) {
-          Ok(_) => {
-            if &data == msg {
-              println!("Reply is ok!");
-            } else {
-              let text = from_utf8(&data).unwrap();
-              println!("Unexpected reply: {}", text);
+          let mut data = [0 as u8; 6]; // using 6 byte buffer
+          match stream.read_exact(&mut data) {
+            Ok(_) => {
+              if &data == msg {
+                println!("Reply is ok!");
+              } else {
+                let text = from_utf8(&data).unwrap();
+                println!("Unexpected reply: {}", text);
+              }
+            }
+            Err(e) => {
+              println!("Failed to receive data: {}", e);
             }
           }
-          Err(e) => {
-            println!("Failed to receive data: {}", e);
-          }
         }
+        Err(e) => {
+          println!("Failed to connect: {}", e);
+        } */
+        stream.write(number_of_actors.as_bytes()).unwrap();
+        stream.flush().unwrap();
       }
       Err(e) => {
         println!("Failed to connect: {}", e);
       }
     }
-    println!("Terminated.");
+    println!("terminated");
   } else {
     // slave is waiting for connections
 
@@ -72,7 +82,9 @@ pub fn run() {
           thread::spawn(move || {
             // connection succeeded
             // handle_master(stream)
-            handle_master_new(stream);
+            handle_messages(stream, |message: &str|{
+              println!("received message {}", message);
+            });
           });
         }
         Err(e) => {
@@ -86,7 +98,7 @@ pub fn run() {
   }
 }
 
-fn handle_master(mut stream: TcpStream) {
+/* fn handle_master(mut stream: TcpStream) {
   let mut data = [0 as u8; 50]; // using 50 byte buffer
   while match stream.read(&mut data) {
     Ok(size) => {
@@ -103,11 +115,20 @@ fn handle_master(mut stream: TcpStream) {
       false
     }
   } {}
-}
+} */
 
-fn handle_master_new(mut conn: TcpStream){
+fn handle_messages<T>(mut conn: TcpStream, f: T)
+where
+  T: FnOnce(&str)
+{
   let mut buffer = [0; 512];
   conn.read(&mut buffer).unwrap();
   let message_as_string = String::from_utf8_lossy(&buffer[..]);
-  println!("got message {}", message_as_string);
+  // TODO: Wow, this looks weird.
+  let message_to_pass_on = &(*message_as_string);
+  f(message_to_pass_on);
+
+  let string_response = "Thanks!";
+  conn.write(string_response.as_bytes()).unwrap();
+  conn.flush().unwrap();
 }
