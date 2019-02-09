d06eff89ca85732274837243487b5f65a562ec69
Benedikt Seifert <bse@lt457.iteratec.de>
2019-02-09 15:38:21 +0100

    Set command for cargo to master and slave is the default

--- src/application/master.rs
+++ src/application/master.rs
@@ -13,16 +13,35 @@ use crate::application::tcp_actor::TcpActor;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
-    let is_master = &args[1];
-    let is_master_bool: bool = is_master.parse().expect("master not parsable");
+    if args.len() <= 1 {
+        run_as_slave();
+    } else {
+        let master_flag: String = args[1].parse().expect("Could not parse flag as intended. Maybe you meant --master?");
+        if master_flag == "master" {
+            run_as_master();
+        } else {
+            run_as_slave();
+        }
+    }
+}
 
-    println!("init");
+pub fn run_as_slave() {
+    println!("Run system as slave");
+    init_system(false);
+}
+
+pub fn run_as_master() {
+    println!("Run system as master");
+    init_system(true);
+}
+
+pub fn init_system(is_master: bool) {
     ActorSystem::start(move || {
         let input_actor = ActorSystem::register_actor(InputActor { context: None }, None);
         let tcp_actor = ActorSystem::register_actor(TcpActor::new(3333), None);
         let config_actor = ActorSystem::register_actor(ConfigActor::new(tcp_actor.clone()), None);
         ActorSystem::register_actor(
-            MasterActor::new(is_master_bool, tcp_actor, Some(input_actor), config_actor),
+            MasterActor::new(is_master, tcp_actor, Some(input_actor), config_actor),
             None,
         );
     });
